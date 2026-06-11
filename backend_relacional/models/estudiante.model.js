7e6e19aa3b75bfafbe92d75b775d151868c21543
Jhoel170 <jhoel.beltran@epn.edu.ec>
2026-06-11 12:56:58 -0500

    sequelize

--- backend_relacional/models/estudiante.model.js
+++ backend_relacional/models/estudiante.model.js
@@ -1,42 +1,46 @@
 const { DataTypes } = require('sequelize');
+const { createSequelize } = require('../config/sequelize.config');
 
-module.exports = (sequelize) => {
-  return sequelize.define('Estudiante', {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      autoIncrement: true,
-      primaryKey: true,
-      validate: {
-        notNull: { msg: "Id is required" }
-      }
-    },
-    nombre: {
-      type: DataTypes.STRING,
-      allowNull: false,
-      validate: {
-        notNull: { msg: "Nombre is required" }
-      }
-    },
-    edad: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      validate: {
-        notNull: { msg: "Edad is required" },
-        isInt: { msg: "Edad debe ser un número entero" },
-        min: { args: [0], msg: "Edad no puede ser negativa" }
-      }
-    },
-    url: {
-      type: DataTypes.STRING,
-      allowNull: true,
-      validate: {
-        isUrl: { msg: "URL no válida" }
-      }
+const sequelize = createSequelize();
+
+const Estudiante = sequelize.define('Estudiante', {
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    autoIncrement: true,
+    primaryKey: true,
+    validate: {
+      notNull: { msg: "Id is required" }
     }
-  });
-};
+  },
+  nombre: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    validate: {
+      notNull: { msg: "Nombre is required" }
+    }
+  },
+  edad: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      notNull: { msg: "Edad is required" },
+      isInt: { msg: "Edad debe ser un número entero" },
+      min: { args: [0], msg: "Edad no puede ser negativa" }
+    }
+  },
+  url: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    validate: {
+      isUrl: { msg: "URL no válida" }
+    }
+  }
+}, {
+  tableName: 'estudiantes'
+});
 
+module.exports = { Estudiante };
 
 /*-- Crear la base de datos
 CREATE DATABASE bdd_epn;
